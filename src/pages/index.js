331075afe2529126ed1c730f08513f23e32545c2
Tucker Whitehouse <tuckerwhitehouse@gmail.com>
2018-02-28 21:12:31 -0500

    Add Facebook & Phone Number

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center' }}>
+  <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
     <img
       style={{ margin: '2rem 0', maxWidth: '80%', maxHeight: '80%', width: '768px' }}
       src={require('assets/logo.svg')}
@@ -10,5 +10,17 @@ export default () => (
     <h1>
       <em>Coming Soon!</em>
     </h1>
+
+    <br />
+    <br />
+
+    <a href="https://www.facebook.com/haywardsofmilford/">Find us on Facebook</a>
+
+    <br />
+    <br />
+
+    <a href="tel:16036728383">
+      Give us a Call<br />(603) 672-8383
+    </a>
   </div>
 )
